5ab5f3183ce28c2cb8eac83a4754c702d290b16e
ninijorbenadze <n_jorbenadze5@cu.edu.ge>
2025-01-29 21:36:40 +0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById('surprise-btn').addEventListener('click', function () {
+    document.getElementById('message').textContent = 'Have a wonderful day filled with success!';
+  });
